f3cd03d5d156cdf491b431a1f4e494a361e88993
Alan Leatherman <alan@theweet.com>
2026-02-13 11:18:44 -0800

    Initial Commit

---
+++ StackEms/StackEmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackEmsApp.swift
+//  StackEms
+//
+//  Created by Alan Leatherman on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct StackEmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StackEmsTests/StackEmsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StackEmsTests.swift
+//  StackEmsTests
+//
+//  Created by Alan Leatherman on 2/13/26.
+//
+
+import Testing
+@testable import StackEms
+
+struct StackEmsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
